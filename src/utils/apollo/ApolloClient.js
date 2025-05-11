8d00c3fe5b11203a770364607f854a4d05db048c
rfdevuser <rakhisfashions@rakhisfashions.com>
2025-05-12 00:44:44 +0530

    Add files via upload

--- src/utils/apollo/ApolloClient.js
+++ src/utils/apollo/ApolloClient.js
@@ -5,7 +5,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 
 const client = new ApolloClient({
-  uri: 'https://www.onatiglobal.com/graphql', 
+  uri: 'https://www.newogwebsite.com/graphql', 
   cache: new InMemoryCache(),
 });
 
