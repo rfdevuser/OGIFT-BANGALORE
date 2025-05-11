5af1670a7e1b9f2103cab26e1b6adb7d058547e6
rfdevuser <rakhisfashions@rakhisfashions.com>
2025-05-12 01:09:58 +0530

    Update ApolloClient.js

--- src/utils/apollo/ApolloClient.js
+++ src/utils/apollo/ApolloClient.js
@@ -5,7 +5,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 
 const client = new ApolloClient({
-  uri: 'https://www.newogwebsite.com?graphql=true', 
+  uri: 'https://www.onatiglobal.com?graphql=true', 
   cache: new InMemoryCache(),
 });
 
