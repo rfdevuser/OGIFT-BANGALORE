1e813faa329e1c25f341bddfa0ab350d40ff6ac9
rfdevuser <rakhisfashions@rakhisfashions.com>
2025-09-26 11:02:09 +0530

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -135,7 +135,7 @@ const Header = () => {
                     }`}
                   />
                 </button>
-                 <a href="https://www.newogwebsite.com/OGIFTVideos/ogift.apk" download="ogift.apk">Download App</a>
+                
                 <nav
         id="navbarCollapse"
         className={`navbar absolute right-0 z-30 w-[250px] rounded border-[.5px] border-body-color/50 bg-white px-6 py-4 duration-300 dark:border-body-color/20 dark:bg-dark lg:visible lg:static lg:w-auto lg:border-none lg:!bg-transparent lg:p-0 lg:opacity-100 ${
@@ -212,6 +212,7 @@ const Header = () => {
 
 
 export default Header;
+
 
 
 
