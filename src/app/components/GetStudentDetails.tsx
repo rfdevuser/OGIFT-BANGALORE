d4f98be3d8b1be8a0884daede8aef13a5df3152d
rfdevuser <rakhisfashions@rakhisfashions.com>
2025-12-15 17:01:00 +0530

    Update GetStudentDetails.tsx

--- src/app/components/GetStudentDetails.tsx
+++ src/app/components/GetStudentDetails.tsx
@@ -37,8 +37,8 @@ if (flag){
        
        <form onSubmit={(e)=>{e.preventDefault();handleSubmit(e);}}>
          <div className="-mx-4 flex flex-wrap ">
-                  <div className="w-full px-10 md:w-1/2 ">
-                    <div className="mb-8 pt-15">
+                  <div className="w-full px-10 md:w-1/2  pt-10">
+                    <div className="mb-8">
                       <label htmlFor="student_id" className="mb-3 block text-sm font-medium text-dark dark:text-white">Enter Student ID</label>
                       <input type="text" id="student_id" name="student_id" value={formData.student_id} onChange={handleChange} placeholder="Enter your ID" className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none" autoComplete="studentID" required />
                       <label>Enter the student ID shared in the admissions email</label>
@@ -55,6 +55,7 @@ if (flag){
     </div>
   )
  }
+
 
 
 
