8c630ea672ffeecc3f46d1b24c42a16e56ffdacd
rfdevuser <rakhisfashions@rakhisfashions.com>
2025-12-15 16:53:59 +0530

    Update GetStudentDetails.tsx

--- src/app/components/GetStudentDetails.tsx
+++ src/app/components/GetStudentDetails.tsx
@@ -36,8 +36,8 @@ if (flag){
        
        
        <form onSubmit={(e)=>{e.preventDefault();handleSubmit(e);}}>
-         <div className="-mx-4 flex flex-wrap p-15">
-                  <div className="w-full px-4 md:w-1/2">
+         <div className="-mx-4 flex flex-wrap ">
+                  <div className="w-full px-4 md:w-1/2 p-15">
                     <div className="mb-8">
                       <label htmlFor="student_id" className="mb-3 block text-sm font-medium text-dark dark:text-white">Enter Student ID</label>
                       <input type="text" id="student_id" name="student_id" value={formData.student_id} onChange={handleChange} placeholder="Enter your ID" className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none" autoComplete="studentID" required />
@@ -55,4 +55,5 @@ if (flag){
     </div>
   )
  }
+
 
